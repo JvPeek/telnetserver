1eedfecce76400051300bdfaeb1e13abcc988a34
Jan <janofthings@gmail.com>
2022-12-10 20:11:35 +0100

    added colon to allowed characters.

--- src/index.js
+++ src/index.js
@@ -234,7 +234,7 @@ function processInput(client, data) {
   if (!client.loggedIn) {
     return;
   }
-  if (/^([a-zA-Z0-9\u0600-\u06FF\u0660-\u0669\u06F0-\u06F9 üöäßÜÖÄẞ\!\?=,-.\\\/]+)$/.test(data)) {
+  if (/^([a-zA-Z0-9\u0600-\u06FF\u0660-\u0669\u06F0-\u06F9 üöäßÜÖÄẞ\!\?:=,-.\\\/]+)$/.test(data)) {
     apps.appList[client.app].processInput(client, data, {"type": DATATYPES.ALPHA});
     return;
   }
